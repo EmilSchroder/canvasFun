e6ca8ec3fa64993a49d0b249a15280bd3fd3027b
EmilSchroder <eschro66@gmail.com>
2018-11-07 16:03:39 +1300

    rotating from canvas origin. Unideal

--- main.js
+++ main.js
@@ -2,20 +2,23 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+document.addEventListener('mousemove', turnChev, false)
 
-drawManyChevs = () => {
+// drawManyChevs = () => {
 
 
-    for (i=0; i < Math.floor(canvas.width/10); i++){
-        for(j=2; j < Math.floor(canvas.height/10); j++){
-            drawChev(i*10,j*10)
-        }
-    }
+//     for (i=0; i < Math.floor(canvas.width/10); i++){
+//         for(j=2; j < Math.floor(canvas.height/10); j++){
+//             drawChev(i*11,j*11)
+//         }
+//     }
     
 
-}
+// }
 
 function drawChev(x,y) {
+    ctx.translate(x+5,y-5)
+    ctx.rotate(0.001)
     ctx.beginPath();
     ctx.moveTo(x,y)
     ctx.lineTo(x+5,y-10)
@@ -24,10 +27,18 @@ function drawChev(x,y) {
     ctx.stroke()
 }
 
+function turnChev(e){
+    let xPos = e.clientX;
+    let yPos = e.clientY;
+
+}
+
 function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
-    drawManyChevs();
+    // drawManyChevs();
+
+    drawChev(100,100)
 
 
     requestAnimationFrame(draw);
